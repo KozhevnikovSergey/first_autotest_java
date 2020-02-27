6843edbcb0ac9be1d4592f30a7a5bcf05b9cd205
Sergey_Kozhevnikov <sergey_kozhevnikov@epam.com>
2020-02-27 14:11:54 +0300

    Added manager for driver

--- src/main/java/driver/Browser.java
+++ src/main/java/driver/Browser.java
@@ -1,6 +1,7 @@
 package driver;
 
 import com.google.common.base.internal.Finalizer;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -21,14 +22,18 @@ public class Browser {
         // Проблема. Берет null
         String browser = System.getProperty("BROWSER");
         if (browser == null){
+            WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
         }
         switch(browser){
             case "CHROME":
+                WebDriverManager.chromedriver().setup();
                 return new ChromeDriver();
             case "FF":
+                WebDriverManager.firefoxdriver().setup();
                 return new FirefoxDriver();
         }
+        WebDriverManager.chromedriver().setup();
         return new FirefoxDriver();
     }
 }
